ce4414f593ae9c0c084c5b44e4be8620630e5921
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2025-06-12 08:53:24 -0600

    feat: improve `Offset2ID`

--- include/REL/Offset2ID.h
+++ include/REL/Offset2ID.h
@@ -16,59 +16,9 @@ namespace REL
 		using const_iterator = typename container_type::const_iterator;
 		using const_reverse_iterator = typename container_type::const_reverse_iterator;
 
-		void load_v2()
-		{
-			const auto iddb = IDDB::GetSingleton();
-			const auto id2offset = iddb->get_id2offset<IDDB::MAPPING>();
-			_offset2id.reserve(id2offset.size());
-			_offset2id.insert(_offset2id.begin(), id2offset.begin(), id2offset.end());
-			std::sort(std::execution::sequenced_policy{}, _offset2id.begin(), _offset2id.end(), [](auto&& a_lhs, auto&& a_rhs) {
-				return a_lhs.offset < a_rhs.offset;
-			});
-		}
-
-		void load_v5()
-		{
-			const auto iddb = IDDB::GetSingleton();
-			const auto id2offset = iddb->get_id2offset<std::uint32_t>();
-			_offset2id.reserve(id2offset.size());
-
-			std::uint64_t id{ 0 };
-			for (auto offset : id2offset) {
-				_offset2id.emplace(_offset2id.end(), value_type{ id++, offset });
-			}
-
-			std::sort(std::execution::sequenced_policy{}, _offset2id.begin(), _offset2id.end(), [](auto&& a_lhs, auto&& a_rhs) {
-				return a_lhs.offset < a_rhs.offset;
-			});
-		}
-
-		[[nodiscard]] std::uint64_t operator()(std::size_t a_offset) const
-		{
-			if (_offset2id.empty()) {
-				REX::FAIL("No Address Library has been loaded!");
-			}
-
-			const value_type elem{ 0, a_offset };
-			const auto       it = std::lower_bound(
-                _offset2id.begin(),
-                _offset2id.end(),
-                elem,
-                [](auto&& a_lhs, auto&& a_rhs) {
-                    return a_lhs.offset < a_rhs.offset;
-                });
-			if (it == _offset2id.end()) {
-				const auto mod = Module::GetSingleton();
-				const auto version = mod->version();
-				REX::FAIL(
-					"Failed to find Address Library ID for offset!\n"
-					"Invalid offset: 0x{:08X}\n"
-					"Game Version: {}",
-					a_offset, version.string());
-			}
-
-			return it->id;
-		}
+		void load_v2();
+		void load_v5();
+		[[nodiscard]] std::uint64_t get_id(std::size_t a_offset) const;
 
 		[[nodiscard]] const_iterator begin() const noexcept { return _offset2id.begin(); }
 		[[nodiscard]] const_iterator cbegin() const noexcept { return _offset2id.cbegin(); }
